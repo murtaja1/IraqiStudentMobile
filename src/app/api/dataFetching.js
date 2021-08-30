b1f594208f3aab6329df1af9da57dbb2ff63d533
Murtaja <murtajaadnan7@gmail.com>
2021-08-30 09:59:57 +0300

    Feat: create a univeristy page

--- src/app/api/dataFetching.js
+++ src/app/api/dataFetching.js
@@ -4,3 +4,13 @@ import Const from "../Const"
 export const fetchData = (setData, page, subUrl) => {
 	axios.get(Const.mainUrl + `${subUrl}?page=1&page_size=${page}`).then((res) => setData(res.data))
 }
+
+export const fetchUniversity = (id, setUniversity) => {
+	axios.get(Const.mainUrl + `universities/${id}`).then(({ data }) => {
+		setUniversity({
+			data: data,
+			tableContent: [data.province, data.president, data.collages_num, data.students_num]
+		}),
+			console.log(data)
+	})
+}
