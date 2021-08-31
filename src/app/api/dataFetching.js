e23ab55ddc5e8a100fca46f68c62da31e4edf75f
Murtaja <murtajaadnan7@gmail.com>
2021-08-31 20:50:48 +0300

    Feat: complete rating compoent and add to university

--- src/app/api/dataFetching.js
+++ src/app/api/dataFetching.js
@@ -21,3 +21,61 @@ export const fetchUniversity = (id, setUniversity) => {
 		})
 	})
 }
+
+export const fetchRating = (setValue, subUrl) => {
+	axios.get(Const.mainUrl + subUrl).then((res) => setValue(res.data.results[0].ave_rating))
+}
+
+export const fetchaAccessToken = async (refresh) => {
+	try {
+		const promise = await axios({
+			method: "post",
+			url: Const.mainUrl + "api/token/refresh/",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			data: {
+				refresh: refresh
+			}
+		})
+		const res = await promise.data
+		return res.access
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+export const fetchaUserId = async (username) => {
+	try {
+		const promise = await axios.get(Const.mainUrl + `users?username=${username}`)
+		const res = await promise.data.results[0].id
+		return res
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+export const sendRating = async (setValue, subUrl, username, rating, refresh, buildingID) => {
+	const access = await fetchaAccessToken(refresh)
+	const userId = await fetchaUserId(username)
+
+	try {
+		const promise = await axios({
+			method: "post",
+			url: Const.mainUrl + subUrl,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + access
+			},
+			data: {
+				rating: rating,
+				user: userId,
+				building: buildingID
+			}
+		})
+		const res = await promise.data
+		setValue(res.ave_rating)
+	} catch (err) {
+		console.log(err.response.data)
+	}
+}
---
+++ src/app/components/AppRating.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react"
+import { View, StyleSheet } from "react-native"
+import { Rating, Text } from "react-native-elements"
+import { useSelector } from "react-redux"
+import { fetchRating, sendRating } from "../api/dataFetching"
+import { navigate } from "../navigation/RootNavigation"
+
+function AppRating({ id }) {
+	const [value, setValue] = useState(0)
+	const state = useSelector((state) => state)
+	const subUrl = `university_ratings?building__id=${id}`
+	const handleSendRating = (rating) => {
+		sendRating(setValue, subUrl, state.username, rating, state.refresh, id)
+	}
+	useEffect(() => {
+		fetchRating(setValue, subUrl)
+	}, [])
+	return (
+		<View>
+			<Text h3 style={styles.value}>
+				{value !== undefined && value}
+			</Text>
+			<Rating
+				readonly={state.username === "" ? true : false}
+				type="custom"
+				startingValue={value}
+				ratingCount={5}
+				imageSize={40}
+				tintColor="white"
+				ratingBackgroundColor="lightgray"
+				onFinishRating={handleSendRating}
+			/>
+			<Text style={styles.infoText}>
+				{state.username === "" ? (
+					<Text>
+						يرجىء
+						<Text onPress={() => navigate("login")} style={{ color: "blue", fontWeight: "bold" }}>
+							تسجيل الدخول
+						</Text>
+						للتقيم
+					</Text>
+				) : (
+					"للتقييم اضغط على النجوم"
+				)}
+			</Text>
+		</View>
+	)
+}
+
+export default AppRating
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "red",
+		padding: 10
+	},
+	infoText: { textAlign: "center", fontSize: 12 },
+	value: { textAlign: "center", color: "gold" }
+})
