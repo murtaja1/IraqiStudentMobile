8062fa8191ce7218de2a0217af7cc0b9563e5dba
Murtaja <murtajaadnan7@gmail.com>
2021-09-01 14:10:38 +0300

    Feat; handle review when not logined in

--- src/app/components/Reviews/Reviews.js
+++ src/app/components/Reviews/Reviews.js
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react"
 import { ActivityIndicator } from "react-native"
 import { View, StyleSheet } from "react-native"
 import { Text, Button } from "react-native-elements"
+import { useSelector } from "react-redux"
 import { fetchData } from "../../api/FetchingData"
 import ReviewChild from "./ReviewChild"
 import ReviewForm from "./ReviewForm"
+import { navigate } from "../../navigation/RootNavigation"
 
 function Reviews({ title, url, empty, id }) {
 	const [reviews, setRewiews] = useState()
 	const [pageNum, setPageNum] = useState(6)
 	const [loading, setLoading] = useState(false)
+	const state = useSelector((state) => state.username)
 	useEffect(() => {
 		const subUrl = url + `&page=1&page_size=${pageNum}`
 		setLoading(true)
@@ -37,9 +40,19 @@ function Reviews({ title, url, empty, id }) {
 						/>
 					)}
 					{reviews.count === 0 && <Text style={styles.empty}>{empty}</Text>}
-					<View style={{ paddingTop: 20 }}>
-						<ReviewForm subUrl={url} id={id} setPageNum={setPageNum} />
-					</View>
+					{state != "" ? (
+						<View style={{ paddingTop: 20 }}>
+							<ReviewForm subUrl={url} id={id} setPageNum={setPageNum} />
+						</View>
+					) : (
+						<Text style={styles.infoText}>
+							يرجىء{" "}
+							<Text onPress={() => navigate("login")} style={styles.loginText}>
+								تسجيل الدخول
+							</Text>{" "}
+							لكتابت مراجعة
+						</Text>
+					)}
 				</>
 			) : (
 				<ActivityIndicator color="blue" />
@@ -61,5 +74,7 @@ const styles = StyleSheet.create({
 		paddingTop: 5,
 		color: "red",
 		textAlign: "center"
-	}
+	},
+	loginText: { color: "blue", fontWeight: "bold", fontSize: 20 },
+	infoText: { color: "red", textAlign: "center", fontSize: 20 }
 })
