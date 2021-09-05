1c5b98dc6ddd66e3e3d7e86bb73b0e08e99c3f10
Murtaja <murtajaadnan7@gmail.com>
2021-09-05 17:46:18 +0300

    Feat: create sum table for collage page

--- src/app/screens/Collage.js
+++ src/app/screens/Collage.js
@@ -4,19 +4,34 @@ import { fetchData } from "../api/FetchingData"
 import CollageTable from "../components/CollageTable"
 import { Text } from "react-native-elements"
 import Reviews from "../components/Reviews/Reviews"
+import SumTable from "../components/SumTable"
 
 function Collage({ route }) {
 	const [collage, setCollage] = useState()
+	const [departments, setDepartments] = useState()
 	useEffect(() => {
 		const { university, collage } = route.params
 		fetchData(setCollage, `collagesdetail?university__name=${university}&name=${collage}`)
 	}, [])
+
+	useEffect(() => {
+		if (collage !== undefined) {
+			fetchData(
+				setDepartments,
+				`department_sum?collage__name=${collage.results[0].name}&collage__university__name=${route.params.university}&page_size=${collage.results[0].departments_num}`
+			)
+		}
+	}, [collage])
 
 	return (
 		<ScrollView contentContainerStyle={{ margin: 10 }}>
 			{collage !== undefined ? (
 				<View style={{ marginBottom: 40 }}>
 					<CollageTable collage={collage.results[0]} university={route.params.university} />
+					{departments !== undefined && (
+						<SumTable collage={collage.results[0]} departments={departments} />
+					)}
+
 					<Text style={styles.title}>معلومات أكثر: </Text>
 					<Text>{collage.results[0].description}</Text>
 					<Reviews
